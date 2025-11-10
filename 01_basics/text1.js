4f361b81e4c6b54b0598ca5559469f6902572226
Mayank <108141566+mayank0213@users.noreply.github.com>
2025-11-10 07:07:14 +0000

    VARIABLES AND COSNTANTS-LEC-1

--- 01_basics/text1.js
+++ 01_basics/text1.js
@@ -1,1 +1,23 @@
-console.log("Hi Mayank")
+const accountId=143
+let accountemail="matyank@.com"
+var acountName="Mayank"
+accountbranch="CANARA BANK"
+
+let accountstate
+
+//Changing the values
+accountemail="hc@hc.com"
+acountName="Satyam"
+accountbranch="Jaipur"  //we can also declare in this way as well
+
+//Trying to update const
+//accountId=321
+console.log(accountId);   //TypeError: Assignment to constant variable.
+
+//To print multiple variables in table format
+console.table([accountemail,acountName,accountbranch,accountstate])
+
+/*var is not to use-->Because of issue in Block/Functional Scope*/
+
+
+
